250918d992cc02026a355e8d0d464259ef192221
Jonathan Wiedemann <contact@freeecad-france.com>
2020-12-10 22:31:47 +0100

    Arduino and python code

---
+++ BoardOnBoat/BoardOnBoat.ino
@@ -0,0 +1,260 @@
+// Board On Boat
+
+// Init
+const int baudRate = 4800; //constant integer to set the baud rate for serial monitor
+
+// IN
+// ANALOG
+const char ThermistorPin = A0;
+const char RudderAnglePin = A1;
+const char PressurePin = A2;
+// DIGITAL
+const int BowLightPin = 8;
+const int PortLightPin = 9;
+const int StarbordLightPin = 10;
+const int SternLightPin = 11;
+
+// Out
+const int RelayBowLight = 2; // Bow
+const int RelayPortLight = 3; // Port
+const int RelayStarbordLight = 4; // Starbord
+const int RelaySternLight = 5; // Stern
+const int RelayHorn = 6; // Horn
+const int RelayAlarm = 7; // Alarm
+
+//Regime moteur temps reel en tour par minute
+int _rpm = 0;
+//temperature of the engine coude d'echappement en degre celsius
+float _temp;
+//oil pressure of the engine
+int _pressure = 0;
+
+//Pour les temporisations
+const unsigned long _periodWriting = 1000;
+unsigned long _lastConsoWriting = 0;
+
+const unsigned long _periodCheckTemp = 1000;
+unsigned long _lastCheckTemp = 0;
+
+const unsigned long _periodCheckPressure = 1000;
+unsigned long _lastCheckPressure = 0;
+
+
+// Alarm control
+boolean alarmState = 0;
+int alarmID = 0;
+void alarmControl(boolean alarmState, int alarmID) {        
+    Serial.print("W");
+    Serial.print(alarmState);
+    Serial.println(alarmID);
+    //digitalWrite(RelayAlarm,HIGH);
+    //digitalWrite(RelayAlarm,LOW);
+}
+
+// RPM Control
+//Cette fonction permet de retourner le regime moteur.
+//0 si le moteur est eteint.
+//Attention, cette fonction dure 100ms au moins ...
+//Pin digitale num 2, donc il y  a la possibilite de gerer les impulsions par interruption si jamais on veut changer.
+const int inpinrpm = 12;
+
+//Constante empirique permettant de calculer les RPM en fonction de la frequence du rupteur :
+//RPM = ALPHA_RPM * freq_rupteur
+//Pour mon moteur Nanni N3.30, cette constante vaut 6.51
+const float ALPHA_RPM = (float)6.51;
+
+
+int readRPM(){
+    float f = 0;
+    float durationPulseLOW = 0;
+    float durationPulseHIGH = 0;
+    unsigned long beginning = millis();
+    unsigned long nowMillis = millis();
+
+    while(nowMillis - beginning < (unsigned long)100){
+        durationPulseLOW += pulseIn(inpinrpm, LOW, (unsigned long)100000);
+        durationPulseHIGH += pulseIn(inpinrpm, HIGH, (unsigned long)100000);
+        f = f + (float)1;
+        nowMillis = millis();
+    }
+
+    //Calcul de la duree moyenne d'une pulsation LOW et HIGH en seconde
+    //Calcul de la duree moyenne d'une pulsation LOW et HIGH en seconde
+    durationPulseLOW /= (f * (float)1000000);
+    durationPulseHIGH /= (f * (float)1000000);
+
+
+    if(durationPulseLOW == 0 || durationPulseHIGH == 0){
+        return 0;
+    }
+    else{
+        //Calcul de la frequence en Hz en faisant l'inverse de la periode egale a la somme des durees des pulsations HIGH et LOW
+        f = (float)1/ (durationPulseLOW + durationPulseHIGH);
+        //Le 6.51 a ete calcule de facon empirique.
+        float rpm = (float)f * ALPHA_RPM;
+
+        //En utilisant la formule trouver sur sonelec, nous aurions :
+        //float rpm = f * (float)40
+        //On arrondi a l'entier le plus proche
+        return (int)(rpm + (float)0.5);
+    }
+}
+
+// Temperature control
+int Vo;
+float R1 = 10000;
+float logR2, R2, T;
+float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+float readTemperature() {
+    // Temperature
+    Vo = analogRead(ThermistorPin);
+    R2 = R1 * (1023.0 / (float)Vo - 1.0);
+    logR2 = log(R2);
+    T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
+    T = T - 273.15;
+    return (float)(T);
+}
+
+// Pressure control
+const float  Offset = 0.397 ;
+float pressureV, pressureP;
+int pressureControl() {
+    //pressureV = analogRead(PressurePin) * 5.00 / 1024;     //Sensor output voltage
+    //pressureP = (pressureV - Offset) * 400;             //Calculate water pressure
+    pressureP = analogRead(PressurePin);
+    _pressure = pressureP;
+    return _pressure;
+}
+
+//  Lights check control
+void lightsCheck() {
+    if (digitalRead(BowLightPin) == HIGH) {
+        alarmControl(1, 5);
+    }
+    if (digitalRead(PortLightPin) == HIGH) {
+        alarmControl(1, 6);
+    }
+    if (digitalRead(StarbordLightPin) == HIGH) {
+        alarmControl(1, 7);
+    }
+    if (digitalRead(SternLightPin) == HIGH) {
+        alarmControl(1, 8);
+    }
+}
+
+// Rudder angle Control
+int rudder;
+void rudderAngleControl() {
+    rudder = analogRead(RudderAnglePin);
+    Serial.print("A");
+    Serial.println((rudder / ( 1023 / 180)) - 90);
+}
+
+
+// Setup the programm
+void setup() {
+    //Initialisation of the serial port
+    Serial.begin(baudRate);
+    
+    pinMode(RelayBowLight, OUTPUT);
+    pinMode(RelayPortLight, OUTPUT);
+    pinMode(RelayStarbordLight, OUTPUT);
+    pinMode(RelaySternLight, OUTPUT);
+    pinMode(RelayHorn, OUTPUT);
+    pinMode(RelayAlarm, OUTPUT);
+
+    //Initialisation of the digital pin
+    pinMode(inpinrpm, INPUT);
+}
+
+// Setup loop
+void loop() {
+    // COMMANDS
+    if(Serial.available() > 0) {
+        //byte command = 0;
+		int command = Serial.read();
+        //Serial.println(command);
+        /**/
+
+        switch (command)
+        {
+        case 49 :
+            digitalWrite(RelayBowLight,HIGH);
+            break;
+        case 50 :
+            digitalWrite(RelayBowLight,LOW);
+            break;
+        case 51 :
+            digitalWrite(RelayPortLight,HIGH);
+            break;
+        case 52 :
+            digitalWrite(RelayPortLight,LOW);
+            break;
+        case 53 :
+            digitalWrite(RelayStarbordLight,HIGH);
+            break;
+        case 54 :
+            digitalWrite(RelayStarbordLight,LOW);
+            break;
+        case 55 :
+            digitalWrite(RelaySternLight,HIGH);
+            break;
+        case 56 :
+            digitalWrite(RelaySternLight,LOW);
+            break;
+        case 57 :
+            digitalWrite(RelayHorn,HIGH);
+            break;
+        case 58 :
+            digitalWrite(RelayHorn,LOW);
+            break;
+        case 65 : //"A" STOP ALARME
+            alarmControl(0, 0);
+            break;
+        case 66 : //"B" TEST ALARME
+            alarmControl(1, 9);
+            break;
+        case 67 : //"C" TEST LIGHT
+            lightsCheck();
+            break;
+        }
+        /**/
+    }
+
+    if(millis() - _lastConsoWriting >= _periodWriting){
+        _lastConsoWriting += _periodWriting;
+        _rpm = readRPM();
+        Serial.print("R");
+        Serial.println(_rpm);
+    }
+
+    if(millis() - _lastCheckTemp >= _periodCheckTemp){
+        _lastCheckTemp += _periodCheckTemp;
+        _temp = readTemperature();
+        Serial.print("T");
+        Serial.println(_temp);
+        if (_temp > 23.0) {
+            alarmControl(1, 1);
+        }
+        else {
+            alarmControl(0, 1);
+        }
+    }
+
+
+    rudderAngleControl();
+
+    if(millis() - _lastCheckPressure >= _periodCheckPressure){
+        _lastCheckPressure += _periodCheckPressure;
+        _pressure = pressureControl();
+        Serial.print("P");
+        Serial.println(_pressure);
+        if (_pressure < 23.0 && _rpm > 0) {
+            alarmControl(1, 2);
+        }
+        else {
+            alarmControl(0, 2);
+        }
+    }
+    
+}
---
+++ BoardOnBoatPyQt.py
@@ -0,0 +1,350 @@
+# coding: utf-8
+
+import sys
+import serial
+import serial.tools.list_ports
+
+
+from PyQt5 import uic, QtSerialPort
+from PyQt5.QtWidgets import QApplication, QPushButton, QTextEdit, QDial
+from PyQt5.QtCore import QFile, QObject, QIODevice, QTimer, pyqtSlot
+
+
+gaugeHtml = """<html><head/><body><p><span style="font-size:16pt;">
+    {gaugeName}<br><span style="font-size:36pt;">
+    {value} {unity}</span></p></body></html>"""
+
+stylesheet = [
+    "background-color: rgb(70, 100, 230);",  # blue
+    "background-color: rgb(230, 70, 70);",  # red
+    "background-color: rgb(20, 220, 20);",  # green
+    "background-color: rgb(255, 255, 255);",  # white
+]
+
+
+
+class Dashboard(QObject):
+    def __init__(self, ui_file, parent=None):
+        super(Dashboard, self).__init__(parent)
+
+        # get ui file and load it
+        ui_file = QFile(ui_file)
+        ui_file.open(QFile.ReadOnly)
+        self.window = uic.loadUi(ui_file)
+        ui_file.close()
+
+        # LOG CONSOLE
+        self.logConsole = self.window.findChild(QTextEdit, "textEdit_Logs")
+
+        # GAUGES
+        self.temperatureGauge = self.window.findChild(QTextEdit, "textEdit_Temperature")
+        self.pressureGauge = self.window.findChild(QTextEdit, "textEdit_Pressure")
+        self.rpmGauge = self.window.findChild(QTextEdit, "textEdit_RPM")
+
+        # LIGHTS BUTTONS
+        ## Bow light
+        self.bowLightButton = self.window.findChild(QPushButton, "pushButton_Bow")
+        self.bowLightButton.clicked.connect(self.ligthsButtonsClicked)
+
+        ## Bord light
+        self.bordLightButton = self.window.findChild(QPushButton, "pushButton_Bord")
+        self.bordLightButton.clicked.connect(self.ligthsButtonsClicked)
+
+        ## Starbord light
+        self.starbordLightButton = self.window.findChild(
+            QPushButton, "pushButton_Starbord"
+        )
+        self.starbordLightButton.clicked.connect(self.ligthsButtonsClicked)
+
+        ## Stern light
+        self.sternLightButton = self.window.findChild(QPushButton, "pushButton_Stern")
+        self.sternLightButton.clicked.connect(self.ligthsButtonsClicked)
+
+        ## All lights
+        self.allLightsButton = self.window.findChild(
+            QPushButton, "pushButton_AllLights"
+        )
+        self.allLightsButton.clicked.connect(self.allLighsButtonClicked)
+
+        ## LIGHTS TEST
+        self.testLightsButton = self.window.findChild(
+            QPushButton, "pushButton_lightsTest"
+        )
+        self.testLightsButton.clicked.connect(self.testLighsButtonClicked)
+        
+        # RUDDER ANGLE INDICATOR
+        self.rubberAngleGauge = self.window.findChild(QDial, "rudderAngle")
+
+        ## ALARM CONTROL
+        self.alarmButton = self.window.findChild(QPushButton, "pushButton_Alarm")
+        self.alarmButton.clicked.connect(self.alarmButtonClicked)
+
+        self.alarmState = False
+        self.tempAlarm = False
+        self.pressureAlarm = False
+        self.alternatorAlarm = False
+        self.lowBatteryAlarm = False
+        self.bowAlarm = False
+        self.portAlarm = False
+        self.starbordAlarm = False
+        self.sternAlarm = False
+
+        # Serial connecxion to  arduino
+        self.portDevice = u"NON CONNECTÉ"
+        self.serialTimer = QTimer()
+        self.serialTimer.setInterval(2000)
+        self.serialTimer.timeout.connect(self.getArduinoSerial)
+        self.serialTimer.start()
+
+        # Timer for logconsole
+        self.logReceive = ""
+        self.consoleTimer = QTimer()
+        self.consoleTimer.setInterval(100)
+        self.consoleTimer.timeout.connect(self.updateLogConsole)
+        self.consoleTimer.start()
+        
+        # Show window
+        self.window.show()
+
+    def getArduinoSerial(self):
+        ports = serial.tools.list_ports.comports(include_links=False)
+        if len(ports) > 0:
+            for port in ports:
+                self.arduino = QtSerialPort.QSerialPort(
+                    port.device,
+                    baudRate=QtSerialPort.QSerialPort.Baud4800,
+                    readyRead=self.receive,
+                )
+                self.arduino.open(QIODevice.ReadWrite)
+                if self.arduino.isOpen():
+                    self.serialTimer.stop()
+                    self.portDevice = port.device
+                    break
+        else:
+            self.portDevice = u"NON CONNECTÉ"
+
+    @pyqtSlot()
+    def receive(self):
+        """called when arduino send serial data"""
+        while self.arduino.canReadLine():
+            text = self.arduino.readLine().data().decode()
+            text = text.rstrip("\r\n")
+            self.logReceive = text
+            #self.logConsole.append(text)
+            if text:
+                self.updateGauges(text)
+        else:
+            self.serialTimer.start()
+
+    def updateGauges(self, data: str):
+        if data[0] == "T":
+            temp = float(data[1:])
+            txt = gaugeHtml.format(gaugeName=u"Température", value=temp, unity=u" °C")
+            self.temperatureGauge.setHtml(txt)
+        elif data[0] == "P":
+            pressure = float(data[1:])
+            txt = gaugeHtml.format(gaugeName=u"Pression", value=pressure, unity=u" BAR")
+            self.pressureGauge.setText(txt)
+        elif data[0] == "R":
+            txt = gaugeHtml.format(
+                gaugeName=u"Vitesse de rotation", value=data[1:], unity=u" RPM"
+            )
+            self.rpmGauge.setText(txt)
+        elif data[0] == "A":
+            self.rubberAngleGauge.setValue(int(data[1:]))
+        elif data[0] == "W":
+            self.alarmsManager(data[1:])
+        else:
+            text = "UNKNOW DATA : " + str(data)
+            self.logReceive = text
+
+    def ligthsButtonsClicked(self):
+        if self.bowLightButton.isChecked():
+            self.arduino.write("1".encode("utf-8"))
+            self.bowLightButton.setStyleSheet(stylesheet[2])
+        else:
+            self.arduino.write("2".encode("utf-8"))
+            self.bowLightButton.setStyleSheet("")
+        if self.bordLightButton.isChecked():
+            self.arduino.write("3".encode("utf-8"))
+            self.bordLightButton.setStyleSheet(stylesheet[2])
+        else:
+            self.arduino.write("4".encode("utf-8"))
+            self.bordLightButton.setStyleSheet("")
+        if self.starbordLightButton.isChecked():
+            self.arduino.write("5".encode("utf-8"))
+            self.starbordLightButton.setStyleSheet(stylesheet[2])
+        else:
+            self.arduino.write("6".encode("utf-8"))
+            self.starbordLightButton.setStyleSheet("")
+        if self.sternLightButton.isChecked():
+            self.arduino.write("7".encode("utf-8"))
+            self.sternLightButton.setStyleSheet(stylesheet[2])
+        else:
+            self.arduino.write("8".encode("utf-8"))
+            self.sternLightButton.setStyleSheet("")
+
+    def allLighsButtonClicked(self):
+        if self.allLightsButton.isChecked():
+            self.bowLightButton.setChecked(True)
+            self.bordLightButton.setChecked(True)
+            self.starbordLightButton.setChecked(True)
+            self.sternLightButton.setChecked(True)
+            self.allLightsButton.setText(u"TOUT ÉTEINDRE")
+        else:
+            self.bowLightButton.setChecked(False)
+            self.bordLightButton.setChecked(False)
+            self.starbordLightButton.setChecked(False)
+            self.sternLightButton.setChecked(False)
+            self.allLightsButton.setText(u"TOUT ALLUMER")
+        self.ligthsButtonsClicked()
+
+    def testLighsButtonClicked(self):
+        self.arduino.write("C".encode("utf-8"))
+
+    def alarmButtonClicked(self):
+        if self.alarmState == True:
+            self.arduino.write("A".encode("utf-8"))
+            self.alarmState = False
+            self.logConsole.setStyleSheet(stylesheet[3])
+            self.alarmButton.setText(u"Tester\nAlarme")
+        else:
+            self.arduino.write("B".encode("utf-8"))
+            self.alarmButton.setText(u"Effacer\nAlarme")
+            self.logConsole.setStyleSheet(stylesheet[1])
+
+    def alarmsManager(self, alarm: str):
+        alarmState = int(alarm[0])
+        alarmId = int(alarm[1:])
+        print(alarmState,alarmId)
+        if alarmId == 0:
+            if alarmState:
+                pass
+                #self.logConsole.append(u"ALARME 0")
+            else:
+                pass
+                #self.logConsole.append(u"PAS D'ALARME")
+            #if alarmState:
+            #    self.logConsole.setStyleSheet(stylesheet[3])
+            #    self.alarmButton.setText(u"Tester\nAlarme")
+        elif alarmId == 1:
+            if alarmState:
+                self.tempAlarm = True
+                self.temperatureGauge.setStyleSheet(stylesheet[1])
+            else:
+                self.tempAlarm = False
+                self.temperatureGauge.setStyleSheet(stylesheet[3])
+        elif alarmId == 2:
+            if alarmState:
+                self.pressureAlarm = True
+                self.pressureGauge.setStyleSheet(stylesheet[1])
+            else:
+                self.pressureAlarm = False
+                self.pressureGauge.setStyleSheet(stylesheet[3])
+        elif alarmId == 3:
+            if alarmState:
+                self.alternatorAlarm = True
+                self.textEdit_Battery.setStyleSheet(stylesheet[1])
+            else:
+                self.alternatorAlarm = False
+                self.textEdit_Battery.setStyleSheet(stylesheet[3])
+        elif alarmId == 4:
+            if alarmState:
+                self.lowBatteryAlarm = True
+                self.textEdit_Battery.setStyleSheet(stylesheet[1])
+            else:
+                self.lowBatteryAlarm = False
+                self.textEdit_Battery.setStyleSheet(stylesheet[3])
+        elif alarmId == 5:
+            if alarmState:
+                self.bowAlarm = True
+                self.bowLightButton.setStyleSheet(stylesheet[1])
+            else:
+                self.bowAlarm = False
+                self.bowLightButton.setStyleSheet(stylesheet[3])
+        elif alarmId == 6:
+            if alarmState:
+                self.portAlarm = True
+                self.bordLightButton.setStyleSheet(stylesheet[1])
+            else:
+                self.portAlarm = False
+                self.bordLightButton.setStyleSheet(stylesheet[3])
+        elif alarmId == 7:
+            if alarmState:
+                self.starbordAlarm = True
+                self.starbordLightButton.setStyleSheet(stylesheet[1])
+            else:
+                self.starbordAlarm = False
+                self.starbordLightButton.setStyleSheet(stylesheet[3])
+        elif alarmId == 8:
+            if alarmState:
+                self.sternAlarm = True
+                self.sternLightButton.setStyleSheet(stylesheet[1])
+            else:
+                self.sternAlarm = False
+                self.sternLightButton.setStyleSheet(stylesheet[3])
+        else:
+            pass
+
+        alarmList = [
+            0,
+            self.tempAlarm,
+            self.pressureAlarm,
+            self.alternatorAlarm,
+            self.lowBatteryAlarm,
+            self.bowAlarm,
+            self.portAlarm,
+            self.starbordAlarm,
+            self.sternAlarm,
+            ]
+        if True in alarmList:
+            #print("there is at least ONE alarm active")
+            self.alarmState = True
+            #self.arduino.write("B".encode("utf-8"))
+            self.logConsole.setStyleSheet(stylesheet[1])
+        else:
+            #print("there is NO alarm active")
+            self.alarmState = False
+            self.arduino.write("A".encode("utf-8"))
+            self.logConsole.setStyleSheet(stylesheet[3])
+
+    def updateLogConsole(self):
+        msg = """<html><head/><body>
+    <p><span style=" font-size:12pt;">Connection série : """
+        
+        msg += self.portDevice
+        msg += "</span></p>"
+
+        msg += """<p><span style=" font-size:12pt;">Alarmes en cours :</span><ul>"""
+        if self.alarmState == False:
+            msg += "<li>Pas d'alarmes en cours</li>"
+        else:
+            if self.tempAlarm:
+                msg += "<li>ALARME TEMPÉRATURE</li>"
+            if self.pressureAlarm:
+                msg += "<li>ALARME PRESSION HUILE</li>"
+            if self.alternatorAlarm:
+                msg += "<li>ALARME ALTERNATEUR</li>"
+            if self.lowBatteryAlarm:
+                msg += "<li>ALARME BATTERIE FAIBLE</li>"
+            if self.bowAlarm:
+                msg += "<li>ALARME FEUX NAVIGATION PROUE</li>"
+            if self.portAlarm:
+                msg += "<li>ALARME FEUX NAVIGATION BABORD</li>"
+            if self.starbordAlarm:
+                msg += "<li>ALARME FEUX NAVIGATION TRIBORD</li>"
+            if self.sternAlarm:
+                msg += "<li>ALARME FEUX NAVIGATION POUPE</li>"
+        msg += "</ul></p>"
+        msg += """<p><span style=" font-size:12pt;">Logs : {log}</span></p></body></html>""".format(log = self.logReceive)
+
+        #<p><span style=" font-size:18pt;">{alarm}</span></p>
+        self.logConsole.setText(msg)
+
+        #<p><span style=" font-size:12pt;">Logs : {log}</span></p>
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    board = Dashboard("BoardOnBoat.ui")
+    sys.exit(app.exec_())
