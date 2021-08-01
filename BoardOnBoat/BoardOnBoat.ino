36e23f38e748c55c5a6f4f3124729b9098c2e74d
Jonathan Wiedemann <contact@freeecad-france.com>
2021-08-01 13:39:54 +0200

    maj calcul temperature

--- BoardOnBoat/BoardOnBoat.ino
+++ BoardOnBoat/BoardOnBoat.ino
@@ -106,16 +106,17 @@ int readRPM(){
 // Temperature control
 //int Vo;
 //temperature of the engine coude d'echappement en degre celsius
+float Vo
 float _temp;
-float R1 = 10000;
+float R1 = 1000;
 float logR2, R2, T;
-float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+float c1 = 0.003194888, c2 = 0.000257961;
 float readTemperature() {
     // Temperature
-    int Vo = analogRead(ThermistorPin);
-    R2 = R1 * (1023.0 / (float)Vo - 1.0);
+    Vo = analogRead(ThermistorPin) * 5.0 / 1024;
+    R2 = Vo * R1 / (5.00 - Vo);
     logR2 = log(R2);
-    T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
+    T = (1.0 / (c1 + c2*logR2));
     T = T - 273.15;
     return (float)(T);
 }
